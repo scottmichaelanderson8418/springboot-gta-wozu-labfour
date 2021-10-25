a0d5883de7ecc60d5cd9e597bf42a839de89ac4b
SCOTT <scottmichaelanderson8418@gmail.com>
2021-10-25 12:45:34 -0400

    cool

--- src/main/java/edu/cpcc/labs/SpringbootGtaWozuLabfourApplication.java
+++ src/main/java/edu/cpcc/labs/SpringbootGtaWozuLabfourApplication.java
@@ -2,7 +2,7 @@ package edu.cpcc.labs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//hey
+//hey hey
 @SpringBootApplication
 public class SpringbootGtaWozuLabfourApplication {
 
